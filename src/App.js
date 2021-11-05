b66500bce39b855a45c776cfadae6275d37ff583
DenysVitvitskyi <78040713+DenysVitvitskyi@users.noreply.github.com>
2021-11-05 21:14:01 +0200

    Home Work 4.1.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+import Statistics from "./components/Statistics/Statistics";
+import FeedbackOptions from "./components/FeedbackOptions/FeedbackOptions";
+import Section from "./components/Section/Section";
+import Notification from "./components/Notification/Notification";
+
+import Wrap from "./App.styled";
+import "./index.css";
+import "./App.css";
+
+export default function App() {
+  const [goodRating, setGoodRating] = useState(0);
+  const [neutralRating, setNeutralRating] = useState(0);
+  const [badRating, setBadRating] = useState(0);
+  const nameButtons = ["good", "neutral", "bad"];
+
+  const changeFeedback = (name) => {
+    switch (name) {
+      case "good":
+        setGoodRating((prevState) => prevState + 1);
+        break;
+
+      case "neutral":
+        setNeutralRating((prevState) => prevState + 1);
+        break;
+
+      case "bad":
+        setBadRating((prevState) => prevState + 1);
+        break;
+
+      default:
+        return;
+    }
+  };
+
+  const countTotalFeedback = () => goodRating + neutralRating + badRating;
+
+  const countPositiveFeedbackPercentage = () => {
+    const positiveFeedback = Number.parseInt(
+      (goodRating * 100) / (goodRating + neutralRating + badRating)
+    );
+    return isNaN(positiveFeedback) ? 0 : `${positiveFeedback} %`;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrap>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={nameButtons}
+          onLeaveFeedback={changeFeedback}
+        />
+      </Section>
+      <Section title="Statistics">
+        {countTotalFeedback() === 0 ? (
+          <Notification />
+        ) : (
+          <Statistics
+            good={goodRating}
+            neutral={neutralRating}
+            bad={badRating}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          />
+        )}
+      </Section>
+    </Wrap>
   );
 }
-
-export default App;
